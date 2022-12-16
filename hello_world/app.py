ec6de22d0450a14195dc0b6be4ec1f35138011bb
DennisA <dennis.apter@awacademy.training>
2022-12-16 10:37:25 +0200

    insane to coocoocs

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
-    response_body = "<h1> Leaves, leaves, leaves! </h1> <h2> We're insane about leaves </h2> <img src='https://i.imgur.com/O1vWos3.jpg' alt='My leaf' />"
+    response_body = "<h1> Leaves, leaves, leaves! </h1> <h2> We're coocoos about leaves </h2> <img src='https://i.imgur.com/O1vWos3.jpg' alt='My leaf' />"
     
     
     return {
